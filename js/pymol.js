c1b86a523def567ed1898a2a2a7420b112715c5d
yakomaxa <47655565+yakomaxa@users.noreply.github.com>
2023-09-28 03:34:22 +0900

    update js

--- js/pymol.js
+++ js/pymol.js
@@ -1,4 +1,4 @@
-let async pymol = (whlurl,OriginX,OriginY,width,height,gui_width,pdb1,filename1) => {
+function pymol(whlurl,OriginX,OriginY,width,height,gui_width,pdb1,filename1){
     let pyodide = await loadPyodide();
     let loaded = false;
     await pyodide.loadPackage(whlurl);
